09ba946cdee6b1a7dadac7ecde74dc2323417f17
Rudis <rudisarg@gmail.com>
2022-07-26 17:56:31 -0400

    created default fn and grid create fn

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
-//function to create grid
+const gridSlider = document.querySelector('.grid-size-slider');
+const gridSize = document.querySelector('.grid-size');
+gridSlider.addEventListener('input', gridCreate);
+const defaultGridCSS = 16;
+const defaultDivAmount = 256;
 
-//function that reads the slider and once user chooses stops make grid based on it
+defaultState();
 
-//function the retrieves color hex code and upon click in the grid change the tiles color
 
-//function that changes color based on rainbow
+//default grid 
+function defaultState() {
+    const defaultGridSize =  document.querySelector(':root');
+    const gridContainer = document.querySelector('.grid');
+    
+    
+    defaultGridSize.style.setProperty('--grid-col', defaultGridCSS)
+    defaultGridSize.style.setProperty('--grid-row', defaultGridCSS)
 
-//
+   
+
+    for(let i = 0; i < defaultDivAmount; i++) {
+        const defaultGridTiles = document.createElement('div');
+        gridContainer.append(defaultGridTiles);
+        defaultGridTiles.classList.add('grid-tile');
+    }
+
+    
+}
+
+//create grid on user click
+function gridCreate(col, row) {
+
+}
