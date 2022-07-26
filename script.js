5716b1bf841b9fdfd0e1b2b31141adccc1081ed4
Rudis <rudisarg@gmail.com>
2022-07-26 04:11:23 -0400

    added comments before creating functions

---
+++ script.js
@@ -0,0 +1,9 @@
+//function to create grid
+
+//function that reads the slider and once user chooses stops make grid based on it
+
+//function the retrieves color hex code and upon click in the grid change the tiles color
+
+//function that changes color based on rainbow
+
+//
