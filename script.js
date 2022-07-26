17d1f7aa75cc5ec3ce377b58f18290729c7411f5
Rudis <rudisarg@gmail.com>
2022-07-26 18:24:03 -0400

    added event listener when user changes scale

--- script.js
+++ script.js
@@ -9,25 +9,56 @@ defaultState();
 
 //default grid 
 function defaultState() {
-    const defaultGridSize =  document.querySelector(':root');
-    const gridContainer = document.querySelector('.grid');
+    const defaultGridSize =  document.querySelector(':root'); // grabbing :root in css file to change the row and column variables
+    const gridContainer = document.querySelector('.grid'); // grabbing container to create tiles inside of it
+    const slider = document.querySelector('.grid-size-slider');
     
     
-    defaultGridSize.style.setProperty('--grid-col', defaultGridCSS)
-    defaultGridSize.style.setProperty('--grid-row', defaultGridCSS)
+    defaultGridSize.style.setProperty('--grid-col', defaultGridCSS) // setting grid-columns to 16
+    defaultGridSize.style.setProperty('--grid-row', defaultGridCSS) // setting grid-rows to 16;
 
    
-
+    // loop through and create default div amount
     for(let i = 0; i < defaultDivAmount; i++) {
         const defaultGridTiles = document.createElement('div');
         gridContainer.append(defaultGridTiles);
         defaultGridTiles.classList.add('grid-tile');
     }
 
+    //event listener to see when user changes grid-size
+    slider.addEventListener('input', () => {
+
+
+        const totalTiles = document.querySelectorAll('.grid-tile');
+        let createTileNumbers = document.querySelector('.grid-size-slider').value;
+        console.log(totalTiles);
+    
+
+        if (createTileNumbers > totalTiles || createTileNumbers < totalTiles) {
+            totalTiles.forEach(tiles => {
+                tiles.remove();
+            })
+        }
+
+        gridCreate(createTileNumbers, createTileNumbers);
+    });
+
+
+
     
 }
 
 //create grid on user click
 function gridCreate(col, row) {
+    const gridContainer = document.querySelector('.grid'); // grabbing container to create tiles inside of it
+    const gridColRow = document.querySelector(':root');
+    gridColRow.style.setProperty('--grid-col', col) // setting grid-columns to 16
+    gridColRow.style.setProperty('--grid-row', row) // setting grid-rows to 16;s
 
+
+    for(let i = 0; i < col*row; i++) {
+        const defaultGridTiles = document.createElement('div');
+        gridContainer.append(defaultGridTiles);
+        defaultGridTiles.classList.add('grid-tile');
+    }
 }
